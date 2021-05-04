3fef0573fc26ec92758caa80b65665d4bcf6b834
Andrei Sas <andreisas007@gmail.com>
2021-05-04 16:27:46 +0300

    prettier code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,16 @@ import { ServerComponent } from './server/server.component';
 
 import { RouterComponent } from './router/router.component';
 import { ServersComponent } from './servers/servers.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, RouterComponent, ServersComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    RouterComponent,
+    ServersComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
